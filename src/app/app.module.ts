b05096b3dd8bb627481bd2de84200df8faaa0854
Praveen Nishanth <praveennishanth@gmail.com>
2018-02-09 15:31:05 +0530

    Angular and Graphcool

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {GraphQLModule} from './apollo.config';
+import { LinkItemComponent } from './link-item/link-item.component';
+import { LinkListComponent } from './link-list/link-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkItemComponent,
+    LinkListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // connection
+    GraphQLModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/link-list/link-list.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {Link} from '../types';
+
+@Component({
+  selector: 'hn-link-list',
+  templateUrl: './link-list.component.html',
+  styleUrls: ['./link-list.component.css']
+})
+export class LinkListComponent implements OnInit {
+  linksToRender: Link[] = [{
+    id: '1',
+    description: 'The Coolest GraphQL Backend 😎',
+    url: 'https://www.graph.cool'
+  }, {
+    id: '2',
+    description: 'The Best GraphQL Client',
+    url: 'http://dev.apollodata.com/'
+  }];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
